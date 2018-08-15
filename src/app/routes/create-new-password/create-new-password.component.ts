0ad5db5b8fc1d31d318f0a600e19e7261dcec1e3
Amanda Iaria <aIaria@mapfreusa.com>
2018-08-15 11:06:28 -0400

    code clean up and just getting ready for e2e

--- src/app/routes/create-new-password/create-new-password.component.ts
+++ src/app/routes/create-new-password/create-new-password.component.ts
@@ -1,9 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { FormBase }               from '../../_models/form-base';
-//import { ForgotPasswordService }  from '../../_services/forms/forgot-password/forgot-password-form/forgot-password.service';
-import { AuthenticationService }      from '../../_services/_iam/authentication-service.service';
-import { CreateNewPasswordFormService }  from '../../_services/forms/forgot-password/create-new-password-form/create-new-password-form.service';
-import { ActivatedRoute }     from "@angular/router";
+import { Component, OnInit, Input }       from '@angular/core';
+import { ActivatedRoute }                 from '@angular/router';
+import { AuthenticationService }          from '../../_services/_iam/authentication-service.service';
+import { CreateNewPasswordFormService }   from '../../_services/forms/forgot-password/create-new-password-form/create-new-password-form.service';
 
 @Component({
   selector: 'app-create-new-password',
@@ -15,63 +13,51 @@ import { ActivatedRoute }     from "@angular/router";
 export class CreateNewPasswordComponent implements OnInit {
   createNewPassword:              any[];
   expiredLink:                    boolean = true;
-  // waiting token api response  
-  waitingForResponse:			  boolean = false;	
   paramSubmission:                any;
   successChangePassword:          boolean = false;
   tooManyAttempts:                boolean = false;
-  	
+  waitingForResponse:             boolean = false;  // waiting token api response
   
-
-  constructor( service: CreateNewPasswordFormService,
+  constructor( 
+    service: CreateNewPasswordFormService,
   	private route: ActivatedRoute,
-  	private authenticationService:  AuthenticationService, ) {
-    this.createNewPassword = service.getInputs();
-    this.route = route
+    private authenticationService:  AuthenticationService 
+  ) {
+      this.createNewPassword = service.getInputs();
+      this.route = route
   }
 
-  checkForConfirmation(event): void{
+  checkForConfirmation(event): void {
     if(event) this.successChangePassword = event;
   }
 
-
-  userConfirmation(event): void 
-  {
-    this.successChangePassword = event;
-  }
-
-  ngOnInit(): void 
-  {
-  
-    this.getTokenfromUrl()
-  } 
-
-  getTokenfromUrl():any{
-  	
-  	this.route.queryParams.subscribe(params=>{
-  		if(params)
-  			return this.isTokenValid(params.token,params.email)
-
-  	})
+  getTokenfromUrl(): boolean{
+  	this.route.queryParams.subscribe(params => {
+      if(params) 
+        return this.isTokenValid(params.token,params.email)
+    });
   	return false
   }
 
-  isTokenValid(token,email):any{
+  isTokenValid(token,email): any{
+    if(!token || !email)
+  	  return false
   	
-  	if(!token || !email)
-  		return false
-  	
-  	this.waitingForResponse  = true
-  	return 	this.authenticationService.tokenVerification(token,email).subscribe((data)=>{
-  		this.expiredLink = false
-  		this.waitingForResponse = false
-  	},(error)=>{
-  		this.expiredLink = true
-  		this.waitingForResponse = false
+    this.waitingForResponse  = true
+    return 	this.authenticationService.tokenVerification(token,email).subscribe((data)=>{
+      this.expiredLink = false
+      this.waitingForResponse = false
+    },(error)=>{
+      this.expiredLink = true
+      this.waitingForResponse = false
+    });
+  }
 
-  	}) 	
-  }	
-} 
+  userConfirmation(event): void {
+    this.successChangePassword = event;
+  }
 
-     
-
+  ngOnInit(): void {
+    this.getTokenfromUrl()
+  }
+}
