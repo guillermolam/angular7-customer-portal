849424316f679bb57ec7d747d07c26a32030ee88
Amanda Iaria <aIaria@mapfreusa.com>
2018-09-06 18:35:20 -0400

    changing env from http to https

--- src/environments/environment.ppr.ts
+++ src/environments/environment.ppr.ts
@@ -1,9 +1,8 @@
 export const environment = {
   production: true,
-  api_gateway_url: 'https://dev.mapfreapis.com:8443',
-  backend_server_url: 'http://mdv-docdevl01:8081',
-  //old identity: 'http://mdv-docdevl01:8082'
-  identity:           'http://mdv-doctest:8082',
-  account:            'http://mdv-doctest:8083',
-  personalpolicy:     'http://mdv-doctest:8084'
+  api_gateway_url:    'https://dev.mapfreapis.com:8443',
+  backend_server_url: 'https://mdv-docdevl01:8081',
+  identity:           'https://mdv-doctest:8082',
+  account:            'https://mdv-doctest:8083',
+  personalpolicy:     'https://mdv-doctest:8084'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@
 export const environment = {
   production: false,
   api_gateway_url:    'https://dev.mapfreapis.com:8443',
-  backend_server_url: 'http://mdv-docdevl01:8081',
-  identity:           'http://mdv-doctest:8082',
-  account:            'http://mdv-doctest:8083',
-  personalpolicy:     'http://mdv-doctest:8084'
+  backend_server_url: 'https://mdv-docdevl01:8081',
+  identity:           'https://mdv-doctest:8082',
+  account:            'https://mdv-doctest:8083',
+  personalpolicy:     'https://mdv-doctest:8084'
 }
