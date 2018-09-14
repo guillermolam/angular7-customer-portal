1f48c9d9cc9eb26a7bd492cf5fc8bc96d305a42b
Amanda Iaria <aIaria@mapfreusa.com>
2018-09-14 15:33:59 -0400

    verify account screen - I forgot that it was on it's own route

--- src/app/routes/verify-account/verify-account.component.ts
+++ src/app/routes/verify-account/verify-account.component.ts
@@ -14,10 +14,47 @@ export class VerifyAccountComponent implements OnInit {
     private userService:              UserService,
   ) { }
 
+  testingData(): User {
+    //This object is for development use. And will be taken out
+    let object = {
+      addPolicyAttempts:          3,
+      firstName:                  'TestFirstName',
+      middleName:                 'TM',
+      lastName:                   'TestLastName',
+      email:                      'testUpdate@email.com',
+      password:                   'abcd12D!',
+      policyDetails: [{
+        InsName1:                 null,
+        effDate:                  '12/12/2018',
+        expDate:                  '12/12/2018',
+        policynumber:             { policynumber: 'BB0490' },
+        policyStatus:             'cancelled',
+        policyType:               'home',
+        status:                   null,
+      },
+      {
+        InsName1:                 null,
+        effDate:                  '12/12/2018',
+        expDate:                  '12/12/2018',
+        policynumber:             { policynumber: '120490' },
+        policyStatus:             'cancelled',
+        policyType:               'auto',
+        status:                   null, 
+      }]
+    };
+    return object;
+  }
+
   ngOnInit() {
     this.userService.$user.subscribe(
       user => {
-        this.user = user;
+        if(user == undefined){
+          this.user = this.testingData();
+          this.userService.updateUser(this.user);
+        }
+        else {
+          this.user = user;
+        }
       }
     );
   }
