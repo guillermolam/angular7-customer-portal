11d7f1eed204555577b6f2122e0133deeccce8b6
Amanda Iaria <aIaria@mapfreusa.com>
2018-09-14 17:11:45 -0400

    clean up for sass files

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -4,25 +4,11 @@ import { User }               from '../_models/user';
 
 @Injectable()
 export class UserService {
-  user :                      User;
-  private messageSource =     new BehaviorSubject<User>(this.user);
-  $user =                     this.messageSource.asObservable();
+  user:                      User;
+  private messageSource=     new BehaviorSubject<User>(this.user);
+  $user=                     this.messageSource.asObservable();
 
   constructor() {}
-
-  /*createUserObject(object): void {
-    this.user = {
-      addPolicyAttempts:                this.userData.addPolicyAttempts +1,
-      firstName:                        object.editFirst_name,
-      middleName:                       object.editMI_name,
-      lastName:                         object.editLast_name,
-      email:                            object.editEmail,
-      policyDetails:                    [{
-        policynumber:                   { policynumber: object.editPolicyNumber }
-      }]
-    };
-    this.updateUser(this.user);
-  }*/
 
   updateUser(user: User) {
     this.messageSource.next(user);
