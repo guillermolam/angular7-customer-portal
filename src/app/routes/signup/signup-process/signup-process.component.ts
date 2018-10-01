57326af19482c9273862d08bb12261784884b3f1
Amanda Iaria <aIaria@mapfreusa.com>
2018-09-30 22:58:47 -0400

    removing test data

--- src/app/routes/signup/signup-process/signup-process.component.ts
+++ src/app/routes/signup/signup-process/signup-process.component.ts
@@ -40,36 +40,7 @@ export class SignupProcessComponent implements OnInit {
 		});
   }
 
-  testingData(): User {
-    //This object is for development use. And will be taken out
-    let object = {
-      addPolicyAttempts:          3,
-      firstName:                  'TestFirstName',
-      middleName:                 'TM',
-      lastName:                   'TestLastName',
-      email:                      'testUpdate@email.com',
-      password:                   'abcd12D!',
-      policyDetails: [{
-        InsName1:                 null,
-        effDate:                  '12/12/2018',
-        expDate:                  '12/12/2018',
-        policynumbers:             { policynumber: 'BB0490' },
-        policyStatus:             'cancelled',
-        policyType:               'home',
-        status:                   null,
-      },
-      {
-        InsName1:                 null,
-        effDate:                  '12/12/2018',
-        expDate:                  '12/12/2018',
-        policynumbers:             { policynumber: '120490' },
-        policyStatus:             'cancelled',
-        policyType:               'auto',
-        status:                   null, 
-      }]
-    };
-    return object;
-  }
+
 
   ngOnInit() {
     
@@ -79,8 +50,6 @@ export class SignupProcessComponent implements OnInit {
       }
     );
     
-    this.user = this.testingData();
-
     this.activatedRoute.params.subscribe((params: Params) => {
       this.whereInTheProcess = params['parm'];
     });
