e7f0d8773b9b49ae7f4602c8e3dc6176abfa1c3e
Amanda Iaria <aIaria@mapfreusa.com>
2018-10-10 13:50:43 -0400

    Amanda: Tweaks to Sass.

--- src/app/components/screens/wallet-passes/onboarding-wallet-modal/onboarding-wallet-modal.component.ts
+++ src/app/components/screens/wallet-passes/onboarding-wallet-modal/onboarding-wallet-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input }         from '@angular/core';
+import { CookieService }                    from 'ngx-cookie-service';
 import { ModalOptions }                     from 'mapfre-design-library/lib/_models/modal-options';
 import { User }                             from '../../../../_models/user';
 
@@ -14,18 +15,25 @@ export class OnboardingWalletModalComponent implements OnInit {
              walletDownloadModalOptions:    ModalOptions;
 
   constructor(
+    private _cookieService:                 CookieService,
   ) { 
     this.walletDownloadModalOptions = new ModalOptions({
-      animatePosition:                    'bottom', 
-      modalId:                            'helpModal',
-      modalTranslateCopy:                 'MODAL_WHERE_CAN_I_TITLE',
-      typeOfModal:                        'wallet',
-      onLoad:                             true
+      animatePosition:                      'bottom', 
+      modalId:                              'helpModal',
+      modalTranslateCopy:                   'MODAL_WHERE_CAN_I_TITLE',
+      typeOfModal:                          'wallet',
+      onLoad:                               true
     });
   }
 
   firstTimeLogInCheck(): void {
+    this.getCookie();
+  }
 
+  getCookie(): void {
+    if ( this._cookieService.get('walletcard') == 'skipped' ||  this._cookieService.get('walletcard') == 'downloaded') {
+      this.walletDownloadModalOptions.onLoad = false;
+    }
   }
 
   goToWalletPassModal(event): void {
