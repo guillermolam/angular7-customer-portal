d36f5f6052217ed3aff7ad4a2aea295053798567
Amanda Iaria <aIaria@mapfreusa.com>
2018-10-26 13:11:33 -0400

    Amanda: created the navigation

--- src/app/components/dashboard/account-main.component.ts
+++ src/app/components/dashboard/account-main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit }      from '@angular/core';
+import { Router }                 from '@angular/router';
+import { ModalOptions }           from 'mapfre-design-library';
 import { User }                   from '../../_models/user';
 import { UserService }            from '../../_services/user.service';
 import { AuthenticationService }  from '../../_services/_iam/authentication-service.service';
@@ -10,11 +12,39 @@ import { AuthenticationService }  from '../../_services/_iam/authentication-serv
 })
 export class AccountMainComponent implements OnInit {
   user:                           User;
+  headerAccountModalOptions:      ModalOptions;
+  hideModalOnRoute:               boolean = false;
 
   constructor(
     private authService:          AuthenticationService,
+    private router:               Router,
     private userService:          UserService
-  ) {}
+  ) {
+    this.headerAccountModalOptions = new ModalOptions({
+      additionalClasses:          'account',
+      additionalButtonClasses:    'header-flat account',
+      animatePosition:            'top',
+      buttonCopy:                 'Account',
+      modalId:                    'accountModal',
+      iconClasses:                'far fa-user-circle',
+      iconFamily:                 'font-awesome',
+      modalTranslateCopy:         'Account',
+      typeOfModal:                'header',
+      screenReader:               true,
+      showIcons:                  true,
+    });
+  }
+
+  hideModalAction(event, activePath): void {
+    const currentUrl = this.router.url;
+    if (event && activePath == currentUrl) {
+      this.hideModalOnRoute = !this.hideModalOnRoute;
+    }
+  }
+
+  resetHideModal(event): void {
+    this.hideModalOnRoute = !this.hideModalOnRoute;
+  }
 
   ngOnInit() {
     this.userService.$user.subscribe(
