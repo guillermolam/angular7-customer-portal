1ab0b8e9c43af2c39489e9bd77c5d6a6cea55e7f
Amanda Iaria <aIaria@mapfreusa.com>
2018-11-05 15:06:11 -0500

    Amanda: added dummy data for dashboard, and lite Angular Templating

--- src/app/components/dashboard/account-main.component.ts
+++ src/app/components/dashboard/account-main.component.ts
@@ -19,6 +19,215 @@ export class AccountMainComponent implements OnInit {
   ) {
   }
 
+  testDatafunction() {
+    let test;
+
+    test = {
+      customer: {
+        firstName: 'FirstName',
+        middleName: 'MiddleName',
+        lastName: 'LastName',
+        policyDetails: [
+          {
+           
+              balance: 0,
+              nextDueDate: '10/28/2018',
+              nextDueAmount: 0,
+              drivers: [
+                {driverName: 'Thomas Kukabango'},
+                {driverName: 'Diane Lansdowne'}
+             ],
+
+              policynumber: {
+                  policynumber: 'QJN952'
+              },
+              agent: {
+                  agentCode: {
+                      code: '434'
+                  },
+                  agentName: 'AAA NORTHEAST INSURANCE AGCY, INC.',
+                  agentNameExt: '',
+                  agentPhone: {
+                      number: '(800) 222-4242'
+                  }
+              },
+              insurer: [
+                {
+                  'firstName': 'JENNIFER',
+                  'middleName': 'L',
+                  'lastName': 'JUDD',
+                  'Insurer Name': 'JENNIFER L JUDD'
+                }
+              ],
+              policyStatus: 'INACTIVE',
+              policyFlags: {
+                isEft: true,
+                isEftEligi: false,
+                isEbillElig: false,
+                isEdfElig: false,
+                isEbill: false,
+                isEdf: false
+              },
+              policyType: 'AUTO',
+              vehicle: [
+                  {
+                    vehicle: '2016 RAM 1500 SLT'
+                  },
+                  {
+                    vehicle: '1998 HONDA CR-V LX'
+                  }
+              ],
+              effDate: '2016-09-10T04:00:00.000+0000',
+              expDate: '2017-09-10T04:00:00.000+0000'
+          },
+          {
+           
+            balance: 1000.99,
+            nextDueDate: '10/28/2018',
+            nextDueAmount: 79.99,
+            drivers: [
+              {driverName: 'Thomas Kukabango'},
+              {driverName: 'Diane Lansdowne'}
+           ],
+
+            policynumber: {
+                policynumber: '66161'
+            },
+            agent: {
+                agentCode: {
+                    code: '434'
+                },
+                agentName: 'AAA NORTHEAST INSURANCE AGCY, INC.',
+                agentNameExt: '',
+                agentPhone: {
+                    number: '(800) 222-4242'
+                }
+            },
+            insurer: [
+              {
+                'firstName': 'JENNIFER',
+                'middleName': 'L',
+                'lastName': 'JUDD',
+                'Insurer Name': 'JENNIFER L JUDD'
+              }
+            ],
+            policyStatus: 'ACTIVE',
+            policyFlags: {
+              isEft: true,
+              isEftEligi: false,
+              isEbillElig: false,
+              isEdfElig: false,
+              isEbill: false,
+              isEdf: false
+            },
+            policyType: 'AUTO',
+            vehicle: [
+                {
+                  vehicle: '2016 RAM 1500 SLT'
+                },
+                {
+                  vehicle: '1998 HONDA CR-V LX'
+                }
+            ],
+            effDate: '2016-09-10T04:00:00.000+0000',
+            expDate: '2017-09-10T04:00:00.000+0000'
+          },
+          {
+           balance: 0,
+            nextDueDate: '10/28/2018',
+            nextDueAmount: 0,
+            address: '110 Strathmore Rd APT #123, Brighton MA 02135',
+            renters: [
+              {renterName: 'Thomas Kukabango'},
+              {renterName: 'Diane Lansdowne'}
+           ],
+            policynumber: {
+                policynumber: '73371'
+            },
+            agent: {
+                agentCode: {
+                    code: '434'
+                },
+                agentName: 'AAA NORTHEAST INSURANCE AGCY, INC.',
+                agentNameExt: '',
+                agentPhone: {
+                    number: '(800) 222-4242'
+                }
+            },
+            insurer: [
+              {
+                'firstName': 'JENNIFER',
+                'middleName': 'L',
+                'lastName': 'JUDD',
+                'Insurer Name': 'JENNIFER L JUDD'
+              }
+            ],
+            policyStatus: 'INACTIVE',
+            policyFlags: {
+              isEft: true,
+              isEftEligi: false,
+              isEbillElig: false,
+              isEdfElig: false,
+              isEbill: false,
+              isEdf: false
+            },
+            policyType: 'PROPERTY',
+            effDate: '2016-09-10T04:00:00.000+0000',
+            expDate: '2017-09-10T04:00:00.000+0000'
+          },
+          {
+          
+            balance: 1000.99,
+            nextDueDate: '10/28/2018',
+            nextDueAmount: 79.99,
+            address: '111 Boston Rd APT #44, Brighton MA 02135',
+
+            renters: [
+              {renterName: 'Thomas Kukabango'},
+              {renterName: 'Diane Lansdowne'}
+          ],
+
+            policynumber: {
+                policynumber: 'Q12345'
+            },
+            agent: {
+                agentCode: {
+                    code: '434'
+                },
+                agentName: 'AAA NORTHEAST INSURANCE AGCY, INC.',
+                agentNameExt: '',
+                agentPhone: {
+                    number: '(800) 222-4242'
+                }
+            },
+            insurer: [
+              {
+                'firstName': 'JENNIFER',
+                'middleName': 'L',
+                'lastName': 'JUDD',
+                'Insurer Name': 'JENNIFER L JUDD'
+              }
+            ],
+            policyStatus: 'ACTIVE',
+            policyFlags: {
+              isEft: true,
+              isEftEligi: false,
+              isEbillElig: false,
+              isEdfElig: false,
+              isEbill: false,
+              isEdf: false
+            },
+            policyType: 'PROPERTY',
+            effDate: '2016-09-10T04:00:00.000+0000',
+            expDate: '2017-09-10T04:00:00.000+0000'
+          },
+        ],
+      }
+    };
+
+    return test;
+  }
+
   ngOnInit() {
     this.userService.$user.subscribe(
       (user) => {
@@ -26,12 +235,8 @@ export class AccountMainComponent implements OnInit {
           this.user = user ;
         }
         else {
-          this.user = {
-            firstName: 'Testone',
-            policyDetails: [{
-              InsName1: 'TestTwo'
-            }]
-          };
+          this.user = this.testDatafunction();
+          console.log(this.user);
         }
       }
     );
