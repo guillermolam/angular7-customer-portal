c270e1096b04df8abd4fd5274ab505e390cdeb6c
unknown <aiaria@mapfreusa.com>
2018-11-12 14:12:26 -0500

    Milind: resolving conflict

--- src/app/_helpers/_pipes/apartment-pipe.pipe.ts
+++ src/app/_helpers/_pipes/apartment-pipe.pipe.ts
@@ -6,16 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ApartmentPipePipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    let output = value;
-    if (args == 'only-apartment') {
-      // outputting just the apartment
-      output = output.slice();
-    }
-    else {
-      // removing the apartment
-      output = output.replace();
-    }
-    return output;
+    
   }
 
 }
