756150447fc70b3cfcb7aaf20210780e4c215cf5
Guillermo Lam <glam@mapfreusa.com>
2018-11-20 13:42:06 -0500

    changing protractor from http to https for mdv-doctest

--- e2e/environment/environment.dev.ts
+++ e2e/environment/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment =  {
-    localHost : 'http://mdv-doctest',
-    mdvDocTest : 'http://mdv-doctest',
+    localHost : 'https://mdv-doctest',
+    mdvDocTest : 'https://mdv-doctest',
     backend_server_url: 'https://mdv-doctest:443/apis'
 }
