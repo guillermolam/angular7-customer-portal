ea757a02bd53a97e96b923ab69212dbd19b5263f
aiaria <aiaria@mapfreusa.com>
2018-12-18 13:31:08 -0500

    Milind: JWT Interceptor

--- src/app/_helpers/jwt.interceptor.ts
+++ src/app/_helpers/jwt.interceptor.ts
@@ -31,7 +31,7 @@ export class JwtInterceptor implements HttpInterceptor {
     return next.handle(request)
     .pipe(catchError((e) => {
       console.log(e);
-      if((e.status == 401 || e.status == 500) && (e.error.error === 'Invalid Request' || e.statusText = "Internal Server Error")){
+      if((e.status == 401 && e.error.error === 'Invalid Request') || e.status == 500){
         this.router.navigate(['/login']);
       }else {
         return throwError(e);
