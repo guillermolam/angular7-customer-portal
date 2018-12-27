6acc0a2ae40845746727099fa4853ffac24de284
unknown <aiaria@mapfreusa.com>
2018-12-27 12:13:20 -0500

    Milind: authenticate service

--- src/app/_services/profile-settings/authenticate-user.service.ts
+++ src/app/_services/profile-settings/authenticate-user.service.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
+import { UserService } from './../user.service';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -6,17 +9,23 @@ import { Injectable } from '@angular/core';
 export class AuthenticateUserService {
 
   constructor(
-
+    private userService: UserService,
+    private http:             HttpClient,
   ) { }
 
   
   authenticateCurrentPassword(password){
-    const url = `${environment.backend_server_url}/identity/users/authenticate`;
-    const body = {
-      email: '',
-      password: ''
-      }
 
+    return this.userService.$user.pipe(map((user)=>{
+      const url = `https://mdv-doctest:8082/identity/users/authenticate`;
+      // const url = `${environment.backend_server_url}/identity/users/authenticate`;
+      const body = {
+      email: user.email.address,
+      password: password
+      }    
+      return this.http.post(url,body);
+    })
+    );    
   }
 
 
