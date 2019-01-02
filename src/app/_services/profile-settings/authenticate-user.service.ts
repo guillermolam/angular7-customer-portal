2c30a0333d27c61e8bc8a3ffc3cd485e83e7be90
unknown <aiaria@mapfreusa.com>
2019-01-02 16:43:57 -0500

    Milind:change password

--- src/app/_services/profile-settings/authenticate-user.service.ts
+++ src/app/_services/profile-settings/authenticate-user.service.ts
@@ -1,3 +1,4 @@
+import { StorageServiceObservablesService } from './../storage-service-observables/storage-service-observables.service';
 import { HttpClient } from '@angular/common/http';
 import { UserService } from './../user.service';
 import { Injectable } from '@angular/core';
@@ -11,23 +12,21 @@ export class AuthenticateUserService {
 
   constructor(
     private userService: UserService,
+    private storageService: StorageServiceObservablesService,
     private http:             HttpClient,
   ) { }
 
   
   authenticateCurrentPassword(password){
  
-      return this.userService.$user.pipe(map((user)=>{
       const url = `https://mdv-doctest:8082/identity/users/authenticate`;
       // const url = `${environment.backend_server_url}/identity/users/authenticate`;
       const body = {
-      email: "testasasf@12312312.com",
+      email: this.storageService.getUserFromStorage(),
       // email: user.email.address,
       password: password
       }    
       return this.http.post(url,body);
-     })
-  );    
   }
 
 
