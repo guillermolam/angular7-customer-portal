345c96aa2f23348ff863621f41a2ea1558beee96
adatla <adatla@mapfreusa.com>
2019-01-23 15:13:59 -0500

    adding url for pcf sso tile

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    backend_auth_server_url: 'https://mdv-doctest:443/auth',
+    backend_auth_server_url: 'https://external-dev.login.sys.nonprod.us-east-1.aws.pcf.mapfreusa.com/oauth/authorize',
     backend_server_url: 'https://mdv-doctest:443/apis'
   };
