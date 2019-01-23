68bdc444c7ddd843ee3c074de77ead1438154c21
adatla <adatla@mapfreusa.com>
2019-01-23 15:33:36 -0500

    adding pcf single sign on changes

--- src/app/auth/auth0-variables.ts
+++ src/app/auth/auth0-variables.ts
@@ -5,7 +5,7 @@ interface AuthConfig {
 }
 
 export const AUTH_CONFIG: AuthConfig = {
-  clientID: 'MrI27GNwdOIxM7wBbtgIimWbdsaVXWBI',
-  domain: 'mapfreb2c-usa.auth0.com',
-  callbackURL: 'https://mdv-docdevl01:4200/login'
+  clientID: '6fa4b695-1c04-4548-a81c-5c329d74eacd',
+  domain: 'external-dev.login.sys.nonprod.us-east-1.aws.pcf.mapfreusa.com',
+  callbackURL: 'https://customer-portal-ui.apps.nonprod.us-east-1.aws.pcf.mapfreusa.com/my-insurance'
 };
