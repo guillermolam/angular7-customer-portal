4233cdc1a44768fed45e2f1272f367e9e5250c77
unknown <aiaria@mapfreusa.com>
2019-01-28 18:15:30 -0500

    Milind:sign up componenet

--- src/app/routes/signup/signup-process/signup-process.component.ts
+++ src/app/routes/signup/signup-process/signup-process.component.ts
@@ -1,56 +1,16 @@
-import { Component, OnInit }              from '@angular/core';
-import { Location }                       from '@angular/common';
-import { ActivatedRoute, Params, NavigationEnd,
-  Router, RoutesRecognized,  }            from '@angular/router';
-import { ModalOptions }                   from 'mapfre-design-library';
-import { filter, pairwise }               from 'rxjs/operators';
-import { AddPolicyService }               from '../../../_services/forms/create-account/add-policy.service';
-import { CreateNewPasswordFormService }   from '../../../_services/forms/forgot-password/create-new-password-form/create-new-password-form.service';
-import { EditPolicyService }              from '../../../_services/forms/create-account/edit-policy.service';
-import { User }                           from '../../../_models/user';
-import { UserService }                    from '../../../_services/user.service';
+import { Component }                  from "@angular/core";
+import { CreateAccountService }       from '../../../_services/forms/create-account/create-account-service.service';
 
 @Component({
-  selector: 'app-signup-process',
-  templateUrl: './signup-process.component.html',
-  styleUrls: ['./signup-process.component.scss']
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.scss"],
+  providers: [ CreateAccountService ]
 })
-export class SignupProcessComponent implements OnInit {
-  previousUrl;
-  user:                                 User  = {};
 
-  constructor(
-    private activatedRoute:             ActivatedRoute,
-    private location:                   Location,
-    private userService:                UserService,
-    private router:                             Router
-  ) {
-   
+export class SignupProcessComponent {
+  inputs: any[];
+ 
+  constructor(service: CreateAccountService) {
+    this.inputs = service.getInputs();
   }
-
-
-
-
-  goBackAPage() {
-    //this.location.back();
-   this.router.navigate(this.previousUrl)
-   // console.log('this.previousUrl', this.previousUrl)
-  }
-
-  ngOnInit() {
-    this.router.events
-      .pipe(filter((e: any) => e instanceof RoutesRecognized),
-          pairwise()
-      ).subscribe((e: any) => {
-          console.log(e[0].urlAfterRedirects); // previous url
-          this.previousUrl = e[0].urlAfterRedirects;
-      });
-
-    this.userService.$user.subscribe(
-      (user) => {
-        this.user = user;
-      }
-    );
-  }
-
 }
--- src/app/routes/signup/signup.component.ts
+++ src/app/routes/signup/signup.component.ts
@@ -1,16 +1,56 @@
-import { Component }                  from "@angular/core";
-import { CreateAccountService }       from '../../_services/forms/create-account/create-account-service.service';
+import { Component, OnInit }              from '@angular/core';
+import { Location }                       from '@angular/common';
+import { ActivatedRoute, Params, NavigationEnd,
+  Router, RoutesRecognized,  }            from '@angular/router';
+import { ModalOptions }                   from 'mapfre-design-library';
+import { filter, pairwise }               from 'rxjs/operators';
+import { AddPolicyService }               from '../../_services/forms/create-account/add-policy.service';
+import { CreateNewPasswordFormService }   from '../../_services/forms/forgot-password/create-new-password-form/create-new-password-form.service';
+import { EditPolicyService }              from '../../_services/forms/create-account/edit-policy.service';
+import { User }                           from '../../_models/user';
+import { UserService }                    from '../../_services/user.service';
 
 @Component({
-  templateUrl: "./signup.component.html",
-  styleUrls: ["./signup.component.scss"],
-  providers: [ CreateAccountService ]
+  selector: 'app-signup-process',
+  templateUrl: './signup-process.component.html',
+  styleUrls: ['./signup-process.component.scss']
 })
+export class SignupComponent implements OnInit {
+  previousUrl;
+  user:                                 User  = {};
 
-export class SignupComponent {
-  inputs: any[];
- 
-  constructor(service: CreateAccountService) {
-    this.inputs = service.getInputs();
+  constructor(
+    private activatedRoute:             ActivatedRoute,
+    private location:                   Location,
+    private userService:                UserService,
+    private router:                             Router
+  ) {
+   
   }
+
+
+
+
+  goBackAPage() {
+    //this.location.back();
+   this.router.navigate(this.previousUrl)
+   // console.log('this.previousUrl', this.previousUrl)
+  }
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((e: any) => e instanceof RoutesRecognized),
+          pairwise()
+      ).subscribe((e: any) => {
+          console.log(e[0].urlAfterRedirects); // previous url
+          this.previousUrl = e[0].urlAfterRedirects;
+      });
+
+    this.userService.$user.subscribe(
+      (user) => {
+        this.user = user;
+      }
+    );
+  }
+
 }
