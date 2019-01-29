8cb9ec02b7a4fb583fe3ed40036a56bf99632bea
aiaria <aiaria@mapfreusa.com>
2019-01-29 12:09:44 -0500

    Milind:resolve errors

--- src/app/routes/signup/signup-process/signup-bop/signup-bop.component.ts
+++ src/app/routes/signup/signup-process/signup-bop/signup-bop.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Location }                       from '@angular/common';
+
 
 @Component({
   selector: 'app-signup-bop',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupBopComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private location:                   Location
+  ) { }
 
   ngOnInit() {
   }
+
+  goBackAPage(){
+    this.location.back();
+  }
+
 
 }
