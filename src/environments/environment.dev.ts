d8b6087c1b9fa0cfc8019bb58e96b5569f350745
Amanda Iaria <aIaria@mapfreusa.com>
2019-01-31 16:09:07 -0500

    Amanda: changed enviroment url

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,9 +3,9 @@ export const environment = {
   backend_auth_server_url: 'http://mdv-doctest02:443/auth',
   backend_server_url:     'http://mdv-doctest02:443/apis',
 
-  backend_server_cu:      'https://mdv-doctest02:8083',
-  backend_server_pp:      'https://mdv-doctest02:8084',
-  backend_server_mk:      'https://mdv-doctest02:8085',
-  backend_server_id:      'https://mdv-doctest02:8087',
-  backend_server_bl:      'https://mdv-doctest02:8086',
+  backend_server_cu:      'https://mdv-doctest:8083',
+  backend_server_pp:      'https://mdv-doctest:8084',
+  backend_server_mk:      'https://mdv-doctest:8085',
+  backend_server_id:      'https://mdv-doctest:8087',
+  backend_server_bl:      'https://mdv-doctest:8086',
 };
