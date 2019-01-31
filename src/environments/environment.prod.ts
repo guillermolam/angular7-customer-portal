2bf3c7c43336574d244e3f4e08c5bc2cb85f1424
aiaria <aiaria@mapfreusa.com>
2019-01-31 11:31:59 -0500

    Milind:change prod evnvironment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   backend_auth_server_url: 'https://10.175.0.168:443/auth',
-  backend_server_url: 'https://10.175.0.168:443/apis'
+  backend_server_url: 'https://10.175.0.168:443/apis',
+  backend_server_mk: 'https://mdv-doctest:8085'
 };
