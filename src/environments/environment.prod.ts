ae6e79b5a78cd41a5d078d3afb02076002898ef8
adatla <adatla@mapfreusa.com>
2019-02-05 15:50:04 -0500

    adding production properties

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backend_server_url: 'https://10.175.0.168:443/apis'
+  backend_server_url: 'https://customer-portal-bff.apps.prd.us-east-1.aws.pcf.mapfreusa.com'
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  backend_server_url: 'https://customer-portal-bff.apps.nonprod.us-east-1.aws.pcf.mapfreusa.com'
+  backend_server_url: 'https://customer-portal-bff.apps.prd.us-east-1.aws.pcf.mapfreusa.com'
 }
